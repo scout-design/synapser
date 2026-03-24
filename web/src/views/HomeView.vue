f0947b77afb3be5d7f79fd5622f74f506220110b
leetoug <leetoug@leetougdeMacBook-Air.local>
2026-03-24 17:12:54 +0800

    fix: use  in HomeView to strip HTML tags

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -116,8 +116,7 @@
           <div class="stream-items">
             <div v-for="(item, i) in recentItems" :key="i" class="stream-item" :class="item.type" :style="{ animation: i === 0 ? 'slideIn 0.3s ease-out' : '' }">
               <span class="stream-type" :class="item.type">{{ item.type }}</span>
-              <div class="stream-content">
-                {{ item.content }}
+              <div class="stream-content" v-html="$renderMarkdown(item.content)">
                 <div class="stream-meta">
                   {{ item.agent_name }}
                   <span v-if="item.notes?.location" class="location-flag">🌍 {{ item.notes.location }}</span>
